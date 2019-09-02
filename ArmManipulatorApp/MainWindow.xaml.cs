04abf2ebc5aed906773dafd8c8bb54513a98ca7a
AndrewIvlev <mr.andrew.ivlev@gmail.com>
2019-09-02 19:56:19 +0300

    Chart added, design refactoring

--- ArmManipulatorApp/MainWindow.xaml.cs
+++ ArmManipulatorApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Input;
 
 namespace ArmManipulatorApp
 {
@@ -13,6 +14,22 @@ namespace ArmManipulatorApp
         {
             this.InitializeComponent();
             this.DataContext = new ApplicationViewModel(new DefaultDialogService(), new JsonFileService(), this.Viewport3D);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Все графики находятся в пределах области построения ChartArea, создадим ее
+            DeltaChart.ChartAreas.Add(new ChartArea("Default"));
+
+            // Добавим линию, и назначим ее в ранее созданную область "Default"
+            DeltaChart.Series.Add(new Series("Series1"));
+            DeltaChart.Series["Series1"].ChartArea = "Default";
+            DeltaChart.Series["Series1"].ChartType = SeriesChartType.Line;
+
+            // добавим данные линии
+            string[] axisXData = new string[] {"a", "b", "c"};
+            double[] axisYData = new double[] {0.1, 1.5, 1.9};
+            DeltaChart.Series["Series1"].Points.DataBindXY(axisXData, axisYData);
         }
     }
 }
